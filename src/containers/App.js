b0f5d6e911d53ccdbe2f83ee7222e05a11e30988
Nikola <76968135+Nikolalazov@users.noreply.github.com>
2021-04-24 21:50:32 +0200

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import withClass from '../hoc/withClass';
 import Aux1 from '../hoc/Aux1';
+import AuthContext from '../context/auth-context';
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +22,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -82,6 +84,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  };
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -92,12 +98,13 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
 
     return (
-      <Aux1>
+    <Aux1>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -105,6 +112,12 @@ class App extends Component {
         >
           Remove Cockpit
         </button>
+      <AuthContext.Provider 
+        value={{
+          authenticated: this.state.authenticated, 
+          login: this.loginHandler
+          }} 
+      >
         {this.state.showCockpit ? (
           <Cockpit
             title={this.props.appTitle}
@@ -114,7 +127,8 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </Aux1>
+      </AuthContext.Provider>
+    </Aux1>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
